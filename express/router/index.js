d24e027b9884f3b3b47546cf12e1e380572c7fde
Mohammed Soheb Shaik <Shaikmdshoaib737@gmail.com>
2025-04-28 17:29:43 -0500

    Third Comment By Shark

--- express/router/index.js
+++ express/router/index.js
@@ -1,7 +1,8 @@
 const express=require('express')
 
-const router=express.Router()
+const router = express.Router()
 
 router.get('/',(req,res)=> {
     res.send("Hello,I am Awesome")
 })
+
--- express/server.js
+++ express/server.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app = express()
 
-
+const routes = require("./router/index")
 
 app.listen(3009, ()=> {console.log("We are listening at port 3009!")})
+
