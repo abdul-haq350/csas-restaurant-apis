87b170aa5627ac0b766f1e5b9eae78c4db8cf8a3
mhdyahiya21 <mhdyahiya21@gmail.com>
2025-04-28 18:01:54 -0500

    fourth comment

--- express/router/index.js
+++ express/router/index.js
@@ -2,7 +2,14 @@ const express=require('express')
 
 const router = express.Router()
 
-router.get('/',(req,res)=> {
-    res.send("Hello,I am Awesome")
+router.get('/biryani',(req,res)=> {
+    res.send("Mashallah,Your Biryani is Awesome")
 })
 
+router.get('/gulab-jamoon',(req,res)=> {
+    res.send("Mashallah,you are as sweet as  gulab jamoon")
+})
+router.get('/chai',(req,res)=> {
+    res.send("Alhamdulillah! Wah Abdul Haq Wah,")
+})
+module.exports=router;
