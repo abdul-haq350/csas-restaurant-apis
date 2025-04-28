4726d2425962c9186df951b7e29c687ac9dc949a
Mohammed Soheb Shaik <Shaikmdshoaib737@gmail.com>
2025-04-28 18:43:48 -0500

    Item's From Shark

--- express/router/index.js
+++ express/router/index.js
@@ -72,5 +72,27 @@ router.get('/chai',(req,res)=> {
     res.send("Alhamdulillah! Wah Abdul Haq Wah,")
 })
 
+router.get('/HaraChicken',(req,res)=> {
+    res.send("Mashallah,Your Hara Chicken is Mind Blowing")
+})
+
+router.get('/Chicken65',(req,res)=> {
+    res.send("Mashallah,Your Chicken 65 is Awesome")
+})
+
+router.get('/LaalChicken',(req,res)=> {
+    res.send("Mashallah,Your Laal Chicken is Perfect")
+
+})
+
+router.get('/ChilliChicken',(req,res)=> {
+    res.send("Mashallah,Your Chilli Chicken is Delicious")
+})
+
+router.get('/ButterChicken',(req,res)=> {
+    res.send("Mashallah,Your Butter Chicken is Crazy")
+})
+
+
 
 module.exports=router;
